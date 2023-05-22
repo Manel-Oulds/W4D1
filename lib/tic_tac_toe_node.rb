931c75e2769962772589d7c1380dedf7e03f37c1
artemplv <artemplv@gmail.com>
2023-05-22 15:52:56 -0400

    in progress

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -3,6 +3,7 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
+  
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -10,7 +11,30 @@ class TicTacToeNode
     @children = []
   end
 
+  def inspect
+    "nmm: #{next_mover_mark}, pmp: #{prev_move_pos}"
+  end
+
   def losing_node?(evaluator)
+    winner = board.winner
+    
+    # p "is board over: #{board.over?}"
+    
+    return true if winner && winner != evaluator 
+    return false if winner == evaluator
+    return false if winner == nil && board.over?
+
+    # p "evaluator: #{evaluator}"
+    # p "next_mover_mark: #{next_mover_mark}"
+    # p "children: #{children}"
+
+    if next_mover_mark != evaluator
+      # p 'first condition'
+      return self.children.all? { |c| c.losing_node?(evaluator) }
+    else
+      # p 'second condition'
+      return self.children.any? { |c| c.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
@@ -22,9 +46,26 @@ class TicTacToeNode
     @board.open_positions.each do |pos|
       new_board = @board.dup
       new_board[pos]= @next_mover_mark
-      node = TicTacToeNode.new(new_board, new_board.next_mark, pos)
+      
+      next_mark = next_mover_mark == :x ? :o : :x
+
+      node = TicTacToeNode.new(new_board, next_mark, pos)
       @children << node
     end
     return @children
   end
 end
+
+# node = TicTacToeNode.new(Board.new, :x)
+# node.board[[0, 0]] = :o
+# node.board[[2, 2]] = :o
+# node.board[[0, 2]] = :o
+# p node.losing_node?(:x)
+
+# node = TicTacToeNode.new(Board.new, :o)
+# node.board[[0, 0]] = :x
+# node.board[[0, 1]] = :x
+# node.board[[0, 2]] = :o
+# node.board[[1, 1]] = :o
+# node.board[[1, 0]] = :x
+# p node.losing_node?(:x)
