9a4f86ec967786f632a6bd11bfe9e4afb8912fe6
Manel-Os <oulds.manel@gmail.com>
2023-05-22 16:40:27 -0400

    Added super_computer_player

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    node.children.each do |c|
+      return c.prev_move_pos if c.winning_node?(mark)
+    end
+    node.children.each do |c|
+      return c.prev_move_pos if !c.losing_node?(mark)
+    end
+    raise RuntimeError.new("No winner ")
+    
+
   end
 end
 
