aa4cbaa38408707206a699d9c0e1c670c357f15e
Manel-Os <oulds.manel@gmail.com>
2023-05-22 16:09:15 -0400

    Add losing_node?

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -18,30 +18,19 @@ class TicTacToeNode
   def losing_node?(evaluator)
     winner = board.winner
     
-    # p "is board over: #{board.over?}"
     
     return true if winner && winner != evaluator 
-    return false if winner == evaluator
-    return false if winner == nil && board.over?
-
-    # p "evaluator: #{evaluator}"
-    # p "next_mover_mark: #{next_mover_mark}"
-    # p "children: #{children}"
-
+    return false if winner == evaluator || (winner == nil && board.over?)
     if next_mover_mark != evaluator
-      # p 'first condition'
-      return self.children.all? { |c| c.losing_node?(evaluator) }
-    else
-      # p 'second condition'
       return self.children.any? { |c| c.losing_node?(evaluator) }
+    else
+      return self.children.all? { |c| c.losing_node?(evaluator) }
     end
   end
 
   def winning_node?(evaluator)
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
   def children
     @board.open_positions.each do |pos|
       new_board = @board.dup
@@ -56,16 +45,3 @@ class TicTacToeNode
   end
 end
 
-# node = TicTacToeNode.new(Board.new, :x)
-# node.board[[0, 0]] = :o
-# node.board[[2, 2]] = :o
-# node.board[[0, 2]] = :o
-# p node.losing_node?(:x)
-
-# node = TicTacToeNode.new(Board.new, :o)
-# node.board[[0, 0]] = :x
-# node.board[[0, 1]] = :x
-# node.board[[0, 2]] = :o
-# node.board[[1, 1]] = :o
-# node.board[[1, 0]] = :x
-# p node.losing_node?(:x)
